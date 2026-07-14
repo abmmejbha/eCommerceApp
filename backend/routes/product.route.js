b42c4eb49c524828a80efc9b524520679d484738
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-07-14 23:09:26 +0600

    updated product route .js with formidable

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -4,18 +4,19 @@ const router = express.Router();
 import {fetchProducts, fetchProductById, addProduct, updateProductDetails, removeProduct} from '../controllers/product.controller.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 import { fetchTopProducts } from '../controllers/product.controller.js';
+import formidable from 'express-formidable';
 
 router
     .route('/')
     .get(fetchProducts)
-    .post(authenticate, authorizeAdmin, multer, addProduct)
+    .post(authenticate, authorizeAdmin, formidable(), addProduct)
     
 router.get("/top", fetchTopProducts);
 
 router
     .route('/:id')
     .get(fetchProductById)
-    .put(authenticate, authorizeAdmin, multer, updateProductDetails)
+    .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
     .delete(authenticate, authorizeAdmin, removeProduct)
 
 

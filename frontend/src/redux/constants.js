f3bf0dcac0f8b0cedaeeff1e71b3086d9dd4526b
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-07-17 23:06:04 +0600

    fixed constant issue

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -3,3 +3,4 @@ export const BASE_URL = "https://user-management-backend-r07v.onrender.com";
 export const USERS_URL = "/api/users";
 export const UPLOAD_URL = "/api/upload";
 export const CATEGORY_URL = "/api/category";
+export const ORDER_URL = "/api/orders";

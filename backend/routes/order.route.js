3eaddc19d77cacb78a098925ff045161cd3c6b15
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-07-17 23:04:05 +0600

    imported orders at order.route.js

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import {createOrder} from "../controllers/order.controller.js"
-import {authenticate} from '../middlewares/authMiddleware.js'
+import {createOrder, findOrderById, getUserOrders, getOrders} from "../controllers/order.controller.js"
+import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 import {getUserOrders, findOrderById} from "../controllers/order.controller.js"
 
 const router = express.Router()
 
 
-router.route('/').post(authenticate, createOrder)
+router.route('/').post(authenticate, createOrder).get(authenticate, authorizeAdmin, getOrders);
 router.route("/mine").get(authenticate, getUserOrders);
 router.route("/:id").get(authenticate, findOrderById);
 

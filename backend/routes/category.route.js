8a511a711549f8ea9b2eed2f448dce0bb6645af7
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-07-19 23:29:02 +0600

    Enhance category routes with CRUD operations

--- backend/routes/category.route.js
+++ backend/routes/category.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { fetchCategories } from '../controllers/category.controller.js';
+import { fetchCategories, createCategory, updateCategory, removeCategory } from '../controllers/category.controller.js';
+import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
 
-router.route('/').get(fetchCategories);
+router.route('/').get(fetchCategories).post(authenticate, authorizeAdmin, createCategory)
+router.route('/:id').put(authenticate, authorizeAdmin, updateCategory).delete(authenticate, authorizeAdmin, removeCategory);
 
 export default router

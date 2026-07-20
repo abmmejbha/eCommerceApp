6dbe3c0ff8248b511f0bf12770cd54a318a1315c
abmmejbha <abmmejbha@gmail.com>
2026-07-20 22:59:49 +0600

    updated constant.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,5 @@
 export const PRODUCT_URL = "/api/products";
-export const BASE_URL = "https://user-management-backend-r07v.onrender.com";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 export const USERS_URL = "/api/users";
 export const UPLOAD_URL = "/api/upload";
 export const CATEGORY_URL = "/api/category";

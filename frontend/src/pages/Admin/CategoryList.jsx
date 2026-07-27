412bcf794dbcc54b527b315775d6bac952a18f1b
abmmejbha <abmmejbha@gmail.com>
2026-07-28 00:13:18 +0600

    added category update feature

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -7,8 +7,10 @@ import CategoryForm from "../../components/CategoryForm";
 
 const CategoryList = () => {
   const { data: categories, isLoading, error } = useFetchCategoriesQuery();
-  const [name, setName] = useState("");
   const [createCategory] = useCreateCategoryMutation();
+  const [name, setName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [updatingName, setUpdatingName] = useState("");
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error?.data?.message || error.error}</p>;
@@ -35,7 +37,16 @@ const CategoryList = () => {
 
       <ul>
         {categories.map((category) => (
-          <li key={category._id}>{category.name}</li>
+          <li 
+          key={category._id}
+          onClick={() => {
+            setSelectedCategory(category)
+            setUpdatingName(category.name)
+          }}
+          style={{cursor: "pointer"}}
+          >
+            {category.name}
+            </li>
         ))}
       </ul>
     </div>

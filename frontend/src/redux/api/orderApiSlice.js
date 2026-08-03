9d8bc88a51cb433dbf7933418f6c6c4f5c2cadb6
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-08-03 23:30:12 +0600

    Add order API endpoints to orderApiSlice

--- frontend/src/redux/api/orderApiSlice.js
+++ frontend/src/redux/api/orderApiSlice.js
@@ -1,1 +1,42 @@
-import
+import { apiSlice } from "./apiSlice";
+import { ORDER_URL } from "../constants";
+
+export const orderApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (order) => ({
+        url: `${ORDER_URL}`,
+        method: "POST",
+        body: order,
+      }),
+      invalidatesTags: ["Order"],
+    }),
+
+    getOrderDetails: builder.query({
+      query: (id) => `${ORDER_URL}/${id}`,
+    }),
+
+    getMyOrders: builder.query({
+      query: () => `${ORDER_URL}/mine`,
+    }),
+
+    getOrders: builder.query({
+      query: () => ORDER_URL,
+      providesTags: ["Order"],
+    }),
+
+    getTotalSalesByDate: builder.query({
+      query: () => `${ORDER_URL}/total-sales-by-date`,
+      providesTags: ["Order"],
+    })
+  }),
+});
+
+export const {
+  useCreateOrderMutation,
+  useGetOrderDetailsQuery,
+  useGetMyOrdersQuery,
+  useGetOrdersQuery,
+  useGetTotalSalesByDateQuery,
+} = orderApiSlice;
+      
